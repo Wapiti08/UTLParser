c3b449c129a304564b79107447bf2fe07f16a906
Wapiti08 <tzrzhuoran@gmail.com>
2023-11-29 19:13:28 +0000

    update utils functions

--- utils/util.py
+++ utils/util.py
@@ -33,10 +33,11 @@ def graph_from_structure_data(bro_log_file: str):
         G = nx.MultiDiGraph()
         edges = []
         # add node with its attribute
-        G.add_node(row['id.orig_h'])
+        G.add_node(row['id.orig_h'], label='orig ip')
         # add its port
         G.nodes[row['id.orig_h']]['port'].append(row['id.orig_p'])
-        G.add_node(row['id.resp_h'])
+        
+        G.add_node(row['id.resp_h'], label='resp ip')
         G.nodes[row['id.resp_h']]['port'].append(row['id.resp_p'])
         # add edge
         G.add_edge(row['id.orig_h'], row['id.resp_h'], time=row['ts'], resp_bytes=row['resp_bytes'], conn_state=row['conn_state'])
@@ -44,19 +45,38 @@ def graph_from_structure_data(bro_log_file: str):
     return list(nx.connected_components(G))
 
 def graph_label(G: nx.Graph, node_indicator:str, att_indicitor:str, edge_indicitor:tuple, label:str):
+    G_label = 0
     if label == '-   Malicious   C&C':
-        # check whether containing specific C&C server Ip
-
-
+        # check whether containing specific C&C server Ip -- node
+        if G.has_node(node_indicator):
+            G_label = 1
+            
     elif label == '-   Malicious   C&C-FileDownload':
         # check specific ip and resp bytes
-
+        condition1 = G.has_edge(*edge_indicitor) and G.edges[edge_indicitor]['resp_bytes'] > 3
+        condition2 = G.has_node(node_indicator)
+        if condition1 and condition2:
+            G_label = 2
+    
     elif label == '-   Malicious   Attack':
+        vul_ports = [37215, 52869, 8081, 666]
         # conn_state or the resp_p ---> vulnerable service
+        for edge in G.edges(data=True):
+            if 'S0' in G.edges[edge]['conn_state']:
+                G_label = 3
+        for node in G.nodes:
+            if node['label'] == 'resp ip' and len(list(set(node['port']) & vul_ports)) > 0:
+                G_label = 3
 
     elif label == '-   Malicious   DDoS':
         # count the edges between two nodes
+        if edges_count(G, edge_indicitor) > att_indicitor:
+            G_label = 4
+    else:
+        print('Currently not support graph label with original label {}'.format(label))
+        exit
 
+    return G_label
 
 def edges_count(G:nx.Graph, edge:tuple):
     '''
