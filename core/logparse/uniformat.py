2f34e29f3b1cedc6547e4f20ce9b2f78a55374be
Wapiti08 <tzrzhuoran@gmail.com>
2024-03-05 17:19:57 +0000

    fix potential bug

--- core/logparse/uniformat.py
+++ core/logparse/uniformat.py
@@ -20,6 +20,17 @@ import random
 import calendar
 import re
 from pathlib import Path
+import logging
+
+# set the configuration
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s [%(levelname)s]: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S'
+                )
+
+# create a logger
+logger = logging.getLogger(__name__)
+
 
 short_month_name = calendar.month_abbr[1:]
 month_regex = "|".join(short_month_name)
@@ -34,19 +45,18 @@ level = "info|warn|error"
 dep_map_dict = {
     "<Month>": ["<Day>"],
     "<Day>":["<Timestamp>"],
-    "<Timestamp>":["<Level>", "<Component>","<Proto>"],
-    "<Component>":["<Proto>"],
+    "<Timestamp>":["<Level>", "<Component>", "<Proto>"],
+    "<Component>":["<Proto>","<Application>"],
     ":":["<Content>"],
-    "<Component>":["[<PID>]","<Content>"],
-    "<Proto>": ["[<PID>]", "<Content>"],
+    "<Proto>": ["[<PID>]", "<Content>",":"],
     "[<PID>]": [":"]
 }
 
 pos_com_mapping = {
-    0: ["<Month>", "<Date>", "<>"],
+    0: ["<Month>", "<Date>"],
     1: ["<Day>", "<Timestamp>"],
     2: ["<Timestamp>"],
-    3: ["<Component>","<Proto>","<Application>"]
+    3: ["<Component>","<Proto>","<Level>","<Application>"]
 }
 
 component_pool = [
@@ -70,12 +80,13 @@ com_rex_mapping = {
     "<Day>":[day_regex],
     "<Date>":[date_regex],
     "<Timestamp>": [timestamp_regex],
-    "<Component>":[r'^[a-zA-Z].*'],
+    "<Component>":[r'^\s*([^ ]+).*'],
     "<Proto>":[r'^.*?(?=\[|:)'],
     "<Application>":[r'^.*?(?=\[|:)'],
     "<Level>":[level],
     "[<PID>]":[r'^\d+$',r'\b\d+\b'],
 }
+
 
 class UniFormat:
     def __init__(self, log_filename: Path):
@@ -96,49 +107,45 @@ class UniFormat:
         tokens = sentence.split(" ",split_num)
         log_format_dict[pos] = []
         # only check ending ":"
-        if not tokens[0].endswith(stop_indictor):
-            for com_name, regex_list in com_rex_mapping.items():
-                print("matching {}".format(tokens[0]))
-                matched = [re.search(regex, tokens[0]) for regex in regex_list]
-                print(matched)
-                if any(item is not None for item in matched):
-                    log_format_dict[pos].append(com_name)
-                    print("current log format dict is:", log_format_dict)
-
-            if len(log_format_dict[pos]) == 0:
-                # if no any match, need to add new regex
-                print("need to add new regex to match: {}".format(tokens[0]))
-                return
-            else:
-                pos += 1
-                return self.com_check(tokens[1], pos, stop_indictor, split_num, log_format_dict)
-                
-        else:
-            # check the component of first part
-            ## remove the stop indicitor part
-            token_no_indicitor = tokens[0].replace(stop_indictor,"")
-            print("replacing token from {} to {}".format(tokens[0], token_no_indicitor))
-            # add the stop indictor as the middle component
-            log_format_dict[pos+1] = [":"]
-            # add default second part as the content
-            log_format_dict[pos+2] = ["<Content>"]
-            for com_name, regex_list in com_rex_mapping.items():
-                matched = [re.search(regex, token_no_indicitor) for regex in regex_list]
-                if any(item is not None for item in matched):
-                    # check whether com_name has exsited in pos
-                    if com_name in log_format_dict[pos]:
-                        continue
-                    else:
+        if stop_indictor ==":":
+            if not tokens[0].endswith(stop_indictor):
+                for com_name, regex_list in com_rex_mapping.items():
+                    matched = [re.search(regex, tokens[0]) for regex in regex_list]
+                    print(matched)
+                    if any(item is not None for item in matched):
                         log_format_dict[pos].append(com_name)
-                        print("current log format dict is:", log_format_dict)
 
-            if len(log_format_dict[pos]) == 0:
-                # if no any match, need to add new regex
-                print("need to add new regex to match: {}".format(tokens[0]))
-                return
+                if len(log_format_dict[pos]) == 0:
+                    return
+                else:
+                    pos += 1
+                    return self.com_check(tokens[1], pos, stop_indictor, split_num, log_format_dict)
+                    
+            else:
+                # check the component of first part
+                ## remove the stop indicitor part
+                token_no_indicitor = tokens[0].replace(stop_indictor,"")
+                print("replacing token from {} to {}".format(tokens[0], token_no_indicitor))
+                # add the stop indictor as the middle component
+                log_format_dict[pos+1] = [":"]
+                # add default second part as the content
+                log_format_dict[pos+2] = ["<Content>"]
+                for com_name, regex_list in com_rex_mapping.items():
+                    matched = [re.search(regex, token_no_indicitor) for regex in regex_list]
+                    if any(item is not None for item in matched):
+                        # check whether com_name has exsited in pos
+                        if com_name in log_format_dict[pos]:
+                            continue
+                        else:
+                            log_format_dict[pos].append(com_name)
+                            print("current log format dict is:", log_format_dict)
+
+                if len(log_format_dict[pos]) == 0:
+                    return
+        else:
+            print("Currently not support stop_indicitor with {}".format(stop_indictor))
 
         return log_format_dict
-
 
     def pos_check(self, pos_com_mapping, maybe_log_format_dict):
         ''' reduce noise and build the graph according to neigh_mapping_dict
@@ -162,47 +169,78 @@ class UniFormat:
             if pos != len(maybe_log_format_dict) - 1:
                 cur_com = com_list[0]
                 if cur_com in dep_map_dict:
-                    print("checking dependency for {}".format(cur_com))
                     deps = dep_map_dict[cur_com]
-                    # filter out any incorrect components from the component_list
+                    # Filter out any incorrect components from the component_list
                     correct_components = [comp for comp in maybe_log_format_dict[pos+1] if comp in deps]
-                    print("right child is {}".format(correct_components))
                     # there is right match and not remove
-                    if len(correct_components)>0 & len(correct_components) == maybe_log_format_dict[pos+1]:
-                        # Update the component_list with correct components
-                        continue
-                    # remove wrong prediction then replace with right dependent component
-                    elif len(correct_components) != maybe_log_format_dict[pos+1]:
-                        maybe_log_format_dict[pos+1] = correct_components
-            
+                    if len(correct_components)>0: 
+                        if len(correct_components) == maybe_log_format_dict[pos+1]:
+                            # Update the component_list with correct components
+                            continue
+                        # remove wrong prediction then replace with right dependent component
+                        else:
+                            maybe_log_format_dict[pos+1] = correct_components
+                    # picked wrong component
+                    else:
+                        return
+                    
+
         log_format_dict = maybe_log_format_dict
         return log_format_dict
 
+    def special_space_remove(self, log_format):
+        if "[PID]" in log_format:
+            # remove the space between PID and previous component
+            log_format = re.sub(r'(.*)\s+(\[<PID>\])', r'\1\2', log_format)
+            # Replace space between [<PID>] and :
+            log_format = re.sub(r'(\[<PID>\])\s+(:)', r'\1\2', log_format)
+        else:
+            # remove the space between : and previous component
+            log_format = re.sub(r'(.*)\s+(:)', r'\1\2', log_format)
+
+        return log_format
+
     def format_ext(self, log_format_dict:dict):
-        ''' extract the shortest path and format log format
+        ''' extract log format
         
         '''
-        # choose the shorted path
-
 
         # format the log format to match the log parser
         com_list = list(log_format_dict.values())
         com_list = [com[0] for com in com_list]
         log_format = " ".join(com_list)
 
-        # remove the space between component and PID
-        # remove the space between PID and :
-        log_format = re.sub(r'(<Component>)\s+(\[<PID>\])', r'\1\2', log_format)
-        # Replace space between [<PID>] and :
-        log_format = re.sub(r'(\[<PID>\])\s+(:)', r'\1\2', log_format)
+        log_format = self.special_space_remove(log_format)
 
         if "[" in log_format:
             log_format = log_format.replace("[","\[")
             log_format = log_format.replace("]","\]")
 
+        logger.info("Extracted log format is: \n {}".format(log_format))
         return log_format
         
+    def final_format(self, log_format_list:list):
+        # choose the shorted path as the most general log format
+        if len(log_format_list) == 1:
+            return log_format_list[0]
+        # all the same formats, choose any one
+        elif len(set(log_format_list)) == 1:
+            return log_format_list[0]
+        # different formats:
+        else:
+            pass
+    
+    def cal_st(self):
+        '''
+        
+        '''
+    
+    def cal_depth(self):
+        '''
+        
+        '''
 
+        
 # the matching format for unstructured logs
 # format_dict = {
 #     "DNS": {
--- test/logformat_generator.py
+++ test/logformat_generator.py
@@ -11,35 +11,40 @@ date_regex=r'\d{4}-\d{2}-\d{2}'
 
 level = "info|warn|error"
 
-# test_logs = {
-#     "Jan 15 10:02:51 dnsmasq[14522]: query[AAAA] dualstack.com.imgix.map.fastly.net from 10.35.35.118",
-#     "Jan 15 10:02:51 dnsmasq[14522]: forwarded dualstack.com.imgix.map.fastly.net to 192.168.255.254",
-#     "Jan 15 10:02:51 dnsmasq[14522]: reply previews.customer.envatousercontent.com is 99.86.237.61",
-#     "Jan 15 10:02:52 dnsmasq[14522]: query[AAAA] tls12.newrelic.com.cdn.cloudflare.net from 10.35.35.118",
-#     "Jan 15 10:02:52 dnsmasq[14522]: forwarded dh4oiqg45nvw7.cloudfront.net to 192.168.255.254"
-# }
+test_logs = {
+    "Jan 15 10:02:51 dnsmasq[14522]: query[AAAA] dualstack.com.imgix.map.fastly.net from 10.35.35.118",
+    "Jan 15 10:02:51 dnsmasq[14522]: forwarded dualstack.com.imgix.map.fastly.net to 192.168.255.254",
+    "Jan 15 10:02:51 dnsmasq[14522]: reply previews.customer.envatousercontent.com is 99.86.237.61",
+    "Jan 15 10:02:52 dnsmasq[14522]: query[AAAA] tls12.newrelic.com.cdn.cloudflare.net from 10.35.35.118",
+    "Jan 15 10:02:52 dnsmasq[14522]: forwarded dh4oiqg45nvw7.cloudfront.net to 192.168.255.254"
+}
 
 test_logs_1 = {
     "Jan 17 17:55:29 intranet-server sshd[13953]: pam_unix(sshd:session): session closed for user hwarren",
     "Jan 17 17:55:29 intranet-server systemd-logind[1011]: Removed session 98."
 }
 
+test_logs_2 = {
+    "Jun 22 04:06:00 combo logrotate: ALERT exited abnormally with [1]",
+    "Jun 22 04:11:42 combo su(pam_unix)[17037]: session opened for user news by (uid=0)"
+}
+
+
 dep_map_dict = {
     "<Month>": ["<Day>"],
     "<Day>":["<Timestamp>"],
-    "<Timestamp>":["<Level>", "<Component>","<Proto>"],
-    "<Component>":["<Proto>"],
+    "<Timestamp>":["<Level>", "<Component>", "<Proto>"],
+    "<Component>":["<Proto>","<Application>"],
     ":":["<Content>"],
-    "<Component>":["[<PID>]","<Content>"],
-    "<Proto>": ["[<PID>]", "<Content>"],
+    "<Proto>": ["[<PID>]", "<Content>",":"],
     "[<PID>]": [":"]
 }
 
 pos_com_mapping = {
-    0: ["<Month>", "<Date>", "<>"],
+    0: ["<Month>", "<Date>"],
     1: ["<Day>", "<Timestamp>"],
     2: ["<Timestamp>"],
-    3: ["<Component>","<Proto>","<Application>"]
+    3: ["<Component>","<Proto>","<Level>","<Application>"]
 }
 
 component_pool = [
@@ -63,7 +68,7 @@ com_rex_mapping = {
     "<Day>":[day_regex],
     "<Date>":[date_regex],
     "<Timestamp>": [timestamp_regex],
-    "<Component>":[r'^[a-zA-Z].*'],
+    "<Component>":[r'^\s*([^ ]+).*'],
     "<Proto>":[r'^.*?(?=\[|:)'],
     "<Application>":[r'^.*?(?=\[|:)'],
     "<Level>":[level],
@@ -80,46 +85,49 @@ def com_check(sentence:str, pos:int, stop_indictor:str, split_num:int, log_forma
     tokens = sentence.split(" ",split_num)
     log_format_dict[pos] = []
     # only check ending ":"
-    if not tokens[0].endswith(stop_indictor):
-        for com_name, regex_list in com_rex_mapping.items():
-            print("matching {}".format(tokens[0]))
-            matched = [re.search(regex, tokens[0]) for regex in regex_list]
-            print(matched)
-            if any(item is not None for item in matched):
-                log_format_dict[pos].append(com_name)
-                print("current log format dict is:", log_format_dict)
-
-        if len(log_format_dict[pos]) == 0:
-            # if no any match, need to add new regex
-            print("need to add new regex to match: {}".format(tokens[0]))
-            return
-        else:
-            pos += 1
-            return com_check(tokens[1], pos, stop_indictor, split_num, log_format_dict)
-            
-    else:
-        # check the component of first part
-        ## remove the stop indicitor part
-        token_no_indicitor = tokens[0].replace(stop_indictor,"")
-        print("replacing token from {} to {}".format(tokens[0], token_no_indicitor))
-        # add the stop indictor as the middle component
-        log_format_dict[pos+1] = [":"]
-        # add default second part as the content
-        log_format_dict[pos+2] = ["<Content>"]
-        for com_name, regex_list in com_rex_mapping.items():
-            matched = [re.search(regex, token_no_indicitor) for regex in regex_list]
-            if any(item is not None for item in matched):
-                # check whether com_name has exsited in pos
-                if com_name in log_format_dict[pos]:
-                    continue
-                else:
+    if stop_indictor ==":":
+        if not tokens[0].endswith(stop_indictor):
+            for com_name, regex_list in com_rex_mapping.items():
+                print("matching {}".format(tokens[0]))
+                matched = [re.search(regex, tokens[0]) for regex in regex_list]
+                print(matched)
+                if any(item is not None for item in matched):
                     log_format_dict[pos].append(com_name)
                     print("current log format dict is:", log_format_dict)
 
-        if len(log_format_dict[pos]) == 0:
-            # if no any match, need to add new regex
-            print("need to add new regex to match: {}".format(tokens[0]))
-            return
+            if len(log_format_dict[pos]) == 0:
+                # if no any match, need to add new regex
+                print("need to add new regex to match: {}".format(tokens[0]))
+                return
+            else:
+                pos += 1
+                return com_check(tokens[1], pos, stop_indictor, split_num, log_format_dict)
+                
+        else:
+            # check the component of first part
+            ## remove the stop indicitor part
+            token_no_indicitor = tokens[0].replace(stop_indictor,"")
+            print("replacing token from {} to {}".format(tokens[0], token_no_indicitor))
+            # add the stop indictor as the middle component
+            log_format_dict[pos+1] = [":"]
+            # add default second part as the content
+            log_format_dict[pos+2] = ["<Content>"]
+            for com_name, regex_list in com_rex_mapping.items():
+                matched = [re.search(regex, token_no_indicitor) for regex in regex_list]
+                if any(item is not None for item in matched):
+                    # check whether com_name has exsited in pos
+                    if com_name in log_format_dict[pos]:
+                        continue
+                    else:
+                        log_format_dict[pos].append(com_name)
+                        print("current log format dict is:", log_format_dict)
+
+            if len(log_format_dict[pos]) == 0:
+                # if no any match, need to add new regex
+                print("need to add new regex to match: {}".format(tokens[0]))
+                return
+    else:
+        print("Currently not support stop_indicitor with {}".format(stop_indictor))
 
     return log_format_dict
 
@@ -136,15 +144,16 @@ def pos_check(pos_com_mapping, maybe_log_format_dict):
         else:
             break
 
-
     log_format_dict = maybe_log_format_dict
     return log_format_dict
 
 def dep_check(dep_map_dict, maybe_log_format_dict):
     # check the dependency pattern
     log_format_dict = {}
+    print("checking dependency for com_map_dict: {}".format(maybe_log_format_dict))
     for pos, com_list in maybe_log_format_dict.items():
         if pos != len(maybe_log_format_dict) - 1:
+            print(com_list)
             cur_com = com_list[0]
             if cur_com in dep_map_dict:
                 print("checking dependency for {}".format(cur_com))
@@ -153,15 +162,35 @@ def dep_check(dep_map_dict, maybe_log_format_dict):
                 correct_components = [comp for comp in maybe_log_format_dict[pos+1] if comp in deps]
                 print("right child is {}".format(correct_components))
                 # there is right match and not remove
-                if len(correct_components)>0 & len(correct_components) == maybe_log_format_dict[pos+1]:
-                    # Update the component_list with correct components
-                    continue
-                # remove wrong prediction then replace with right dependent component
-                elif len(correct_components) != maybe_log_format_dict[pos+1]:
-                    maybe_log_format_dict[pos+1] = correct_components
-        
+                if len(correct_components)>0: 
+                    if len(correct_components) == maybe_log_format_dict[pos+1]:
+                        # Update the component_list with correct components
+                        continue
+                    # remove wrong prediction then replace with right dependent component
+                    else:
+                        maybe_log_format_dict[pos+1] = correct_components
+                # picked wrong component
+                else:
+                    print("no dependency found for {}".format(cur_com))
+                    # remove the cur_com
+                    # com_list.remove(cur_com)
+                    return
+                    
+
     log_format_dict = maybe_log_format_dict
     return log_format_dict
+
+def special_space_remove(log_format):
+    if "[PID]" in log_format:
+        # remove the space between PID and previous component
+        log_format = re.sub(r'(.*)\s+(\[<PID>\])', r'\1\2', log_format)
+        # Replace space between [<PID>] and :
+        log_format = re.sub(r'(\[<PID>\])\s+(:)', r'\1\2', log_format)
+    else:
+        # remove the space between : and previous component
+        log_format = re.sub(r'(.*)\s+(:)', r'\1\2', log_format)
+
+    return log_format
 
 def format_ext(log_format_dict:dict):
     ''' extract the shortest path and format log format
@@ -175,11 +204,7 @@ def format_ext(log_format_dict:dict):
     com_list = [com[0] for com in com_list]
     log_format = " ".join(com_list)
 
-    # remove the space between component and PID
-    # remove the space between PID and :
-    log_format = re.sub(r'(<Component>)\s+(\[<PID>\])', r'\1\2', log_format)
-    # Replace space between [<PID>] and :
-    log_format = re.sub(r'(\[<PID>\])\s+(:)', r'\1\2', log_format)
+    log_format = special_space_remove(log_format)
 
     if "[" in log_format:
         log_format = log_format.replace("[","\[")
@@ -188,8 +213,8 @@ def format_ext(log_format_dict:dict):
     return log_format
     
 
-
-for sentence in test_logs_1:
+log_format_dict_list = []
+for sentence in test_logs_2:
     print("generating log format for: \n {}".format(sentence))
     log_format_dict = {}
     stop_indictor = ":"
@@ -199,7 +224,11 @@ for sentence in test_logs_1:
     print("the potential format dict is: \n {}".format(update_log_format_dict))
     log_format_dict_right_pos = pos_check(pos_com_mapping, update_log_format_dict)
     print("matched log format in right position: {}".format(log_format_dict_right_pos))
-    log_format_dict = dep_check(dep_map_dict, log_format_dict_right_pos)
-    print("matched right format dict is: {}".format(log_format_dict))
-    print("generated log format is: {}".format(format_ext(log_format_dict)))
+    res = dep_check(dep_map_dict, log_format_dict_right_pos)
+    if res:
+        print("matched right format dict is: {}".format(res))
+        print("generated log format is: {}".format(format_ext(res)))
 
+        log_format_dict_list.append(format_ext(res))
+
+print(log_format_dict_list)
